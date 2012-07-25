6cb47280fd9d71d1f11e2ac41e2dc0226547e980
Jack Wilsdon <jack.wilsdon@gmail.com>
2012-07-25 14:52:20 +0100

    Added object reference to output

--- plugins/example.php
+++ plugins/example.php
@@ -7,9 +7,8 @@ class example implements iPlugin
 	}
 
 	public function onSpeak($sender, $message, $rawdata = null, $connection, $config) {
-		echo $sender." said ".$message;
 		if ($message == $config->prefix."about") {	
-			$connection->sendMessage($sender, "I'm coded in PHP, and my source is available at ".$url);
+			$connection->sendMessage($sender, "I'm coded in PHP, and my source is available at ".$this->url);
 		}
 	}
 
