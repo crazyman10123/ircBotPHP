5364355f0ed116c4fb216b9e10bca729b7ecf99b
Jack Wilsdon <jack.wilsdon@gmail.com>
2012-07-25 22:52:58 +0100

    Fixed major bug in message recognition

--- main.php
+++ main.php
@@ -21,17 +21,17 @@ if ($connection->connection == false) {
 
 while(1) {
 	while($data = fgets($connection->connection, 128)) {
-	
+
 		// Check for MOTD
 		if (strstr($data, "End of /MOTD command")) {
 			echo "Joined server!\n";
 		}
-		
+
 		// Check for speaking
 		if ((strstr($data, " PRIVMSG ".$config->channel) || strstr($data, " PRIVMSG ".$config->nick))) {
 			$split_data = explode(" PRIVMSG ", $data);
 			$message = str_replace(array(chr(10), chr(13)), '', $split_data[1]);
-			$message = explode($config->channel." :", $message);
+			$message = explode(" :", $message);
 			$sender = $message[0];
 			if ($sender == $config->nick) {
 				$sender = explode("!", $data);
@@ -42,14 +42,14 @@ while(1) {
 				$loadedPlugin->onSpeak($sender, $message, $data, $connection, $config);
 			}
 		}
-		
+
 		// Ping
 		$ex = explode(' ', $data);
 		if($ex[0] == "PING"){
 		fputs($connection->connection, "PONG ".$ex[1]."\r\n");
 		echo "Server sent ping\n";
 		}
-		
+
 	}
 }
 ?>
