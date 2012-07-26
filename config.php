01ffbbbc2573b98cabb5242d54f5515692cc187c
Jack Wilsdon <jack.wilsdon@gmail.com>
2012-07-26 13:38:11 +0100

    Updated config with isup plugin

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ class Config
 	public $server = ""; // IRC server
 	public $nick = "ircBotV2"; // bot's nickname
 	public $channel = ""; // channel to join
-	public $plugins = array("lmgtfy", "defaultCommands", "googl", "example", "lmgtfy"); // plugins to load
+	public $plugins = array("defaultCommands", "googl", "lmgtfy",  "isup", "examples"); // plugins to load
 	public $prefix = "-"; // prefix to call the bot by. e.g. -plugins
 	public $owner = ""; // admin able to access all defaultCommands
 	public $bye = ""; // user with 'bye' permissions
