2600131b096843d2d23f141f4aaa2ddee18f8a51
Jack Wilsdon <jack.wilsdon@gmail.com>
2012-07-29 00:21:04 +0100

    Added extra stuff to suit reload

--- Configuration.php
+++ Configuration.php
@@ -9,17 +9,18 @@ class Configuration
 	public $prefix = "-"; // The Prefix to use when running commands
 	public $authUser = ""; // Your IRC username
 	public $authPass = ""; // A password to use with the -auth command
-	
+
 	public $nickServ = ""; // The nickserv password for the username, if it is registered (optional)
 	
 	public $plugins = array("authService", "defaultCommands", "filter", "lmgtfyService", "shortenService", "isupService", "quotes", "xkcdComics", "pastebinService", "guessGame"); // A list of plugins to enable
 	
 	public $debug = false; // Debug mode - DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING!
 	
-	public $version = "ircBot v3 unstable"; // DO NOT EDIT
+	public $version = "ircBot v3.5 unstable"; // DO NOT EDIT
 
 	// DO NOT MODIFY
 	public $allCommands = array();
 	public $ownerHost = null;
+	public $loadedPlugins = array();
 }
 ?>
