3dd7483623f1f7933a091a4472ab29be96c40419
Jack Wilsdon <jack.wilsdon@gmail.com>
2012-07-29 01:04:44 +0100

    Removed unneccecary junk

--- Configuration.php
+++ Configuration.php
@@ -3,13 +3,13 @@ class Configuration
 {
 	public $timeZone = "GMT"; // Your timezone (for CTCP TIME)
 
-	public $server = "irc.freenode.net"; // The IRC server to connect to
-	public $nick = "DeadBot5"; // The Nick that the bot should use
-	public $channel = "##super"; // The Channel that the bot should join
+	public $server = ""; // The IRC server to connect to
+	public $nick = ""; // The Nick that the bot should use
+	public $channel = ""; // The Channel that the bot should join
 	public $prefix = "-"; // The Prefix to use when running commands
-	public $authMask = ":tcial!~tcial@host-87-75-138-84.dslgb.com"; // Your IRC hostmask (with semicolon at beginning)
+	public $authMask = ""; // Your IRC hostmask (with semicolon at beginning)
 
-	public $nickServ = "jack"; // The nickserv password for the username, if it is registered (optional)
+	public $nickServ = ""; // The nickserv password for the username, if it is registered (optional)
 	
 	public $plugins = array("authService", "defaultCommands", "filter", "lmgtfyService", "shortenService", "isupService", "quotes", "xkcdComics", "pastebinService", "guessGame"); // A list of plugins to enable
 	
